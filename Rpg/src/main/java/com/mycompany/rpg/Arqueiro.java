85cc994d5eb7790f861dea72cf5549af119ccfe2
NatanOli <84872836+NatanOli@users.noreply.github.com>
2023-09-14 22:13:30 -0300

    Add files via upload

---
+++ Rpg/src/main/java/com/mycompany/rpg/Arqueiro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.rpg;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Arqueiro extends Personagem{
+
+    public Arqueiro(String Nome, double danoAtaque, double Vida) {
+        super(Nome, danoAtaque, Vida);
+    }
+
+    
+}
